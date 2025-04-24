aecaca2969bb624f3b73a72421ab3546f5a67ad6
Daria Deriagina <w0862447@myscc.ca>
2025-04-24 10:19:36 -0400

    Created Session data class for workout tracking

---
+++ app/src/main/java/com/example/fitnesstracker/Session.kt
@@ -0,0 +1,11 @@
+package com.example.fitnesstracker
+
+
+// This is your data class that represents one workout session
+data class Session(
+    val date: String,          // Example: "2025-04-23"
+    val workoutType: String,   // Example: "Leg Day"
+    val duration: String       // Example: "45 minutes"
+)
+
+//Data classes in Kotlin – KotlinLang
